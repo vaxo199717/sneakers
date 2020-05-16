0a19ebc4b0f944f3f478ed35639fbc3b49892388
vaxo199717 <vaxoozgebishvili.27@gmail.com>
2020-05-17 00:44:56 +0400

    qwe

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MainService } from '../main.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'header',
@@ -12,33 +8,13 @@ import { Observable } from 'rxjs';
 })
 export class HeaderComponent implements OnInit {
 
-  query: string = '';
-  queryCtrnl: FormControl = new FormControl('');
 
   constructor(
-   private router: Router,
-   private activatedRoute: ActivatedRoute
+  
   ) { }
 
   ngOnInit(): void {
-      this.queryCtrnl
-          .valueChanges
-          .pipe(
-            debounceTime(500),
-            distinctUntilChanged()
-          ).subscribe(
-             query => this.updateQueryParams(query)
-          )
+   
   }
-
-  updateQueryParams(query: string) {
-    this.router.navigate(
-      [],
-      {
-        relativeTo: this.activatedRoute,
-        queryParams: { query },
-        queryParamsHandling: 'merge'
-      });
-  };
 
 }
--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-items',
@@ -7,11 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
-  constructor() { 
-    
-  }
+  query: string = '';
+  queryCtrnl: FormControl = new FormControl('');
+
+  constructor(
+   private router: Router,
+   private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+      this.queryCtrnl
+          .valueChanges
+          .pipe(
+            debounceTime(500),
+            distinctUntilChanged()
+          ).subscribe(
+             query => this.updateQueryParams(query)
+          )
   }
 
+  updateQueryParams(query: string) {
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.activatedRoute,
+        queryParams: { query },
+        queryParamsHandling: 'merge'
+      });
+  };
 }
